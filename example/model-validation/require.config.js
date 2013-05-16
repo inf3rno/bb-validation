df3bd123bc8f9a5a24b6b09ee7406ed1f16abfa4
inf3rno <laszlo.janszky@gmail.com>
2013-05-17 01:12:01 +0200

    Modified model validation example, now it runs in browser too.

---
+++ example/model-validation/require.config.js
@@ -0,0 +1,34 @@
+require.config({
+    baseUrl: "/example",
+    paths: {
+        backbone: "lib/backbone",
+        underscore: "lib/underscore",
+        jquery: "lib/jquery-1.8.3.min",
+        domReady: "lib/domReady",
+        backboneValidator: "../src/backbone-validator",
+        backboneValidatorBasicTests: "../src/backbone-validator-basic-tests",
+        example: "model-validation/index"
+    },
+    shim: {
+        example: {
+            deps: [
+                "backboneValidatorBasicTests",
+                "domReady!"
+            ]
+        },
+        backboneValidator: {
+            deps: ["backbone"]
+        },
+        backboneValidatorBasicTests: {
+            deps: ["backboneValidator"]
+        },
+        backbone: {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+        underscore: {
+            exports: "_"
+        }
+    }
+
+});
